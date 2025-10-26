d3f9b4ec28da3103bd8119c89a87b68acaad4ae9
Muhammad Faisal <faisalmd25121999@gmail.com>
2025-10-26 17:59:51 +0530

    global cors

--- src/server.js
+++ src/server.js
@@ -25,6 +25,19 @@ app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // or specific origin(s)
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    // End preflight requests quickly
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 app.get("/", (_req, res) => {
   res.status(200).json({
     status: "OK",
